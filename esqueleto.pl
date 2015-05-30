ccdc223986c0956b1d2e92b659cad0644e6cae7b
agajda <agajda@gmail.com>
2015-05-29 21:41:13 -0300

    Ejercicios del 1 al 4

--- esqueleto.pl
+++ esqueleto.pl
@@ -5,25 +5,27 @@
 %% Ejercicio 1
 %% tablero(+Filas,+Columnas,-Tablero) instancia una estructura de tablero en blanco
 %% de Filas x Columnas, con todas las celdas libres.
-tablero(_,_,_).
-
+tablero(1,C,T) :- length(COLS,C), T=[COLS].
+tablero(F,C,T) :- F>1, C>0, F2 is F-1, length(COLS,C), tablero(F2,C,T2), T=[COLS|T2].
 
 %% Ejercicio 2
 %% ocupar(+Pos,?Tablero) será verdadero cuando la posición indicada esté ocupada.
-ocupar(_,_).
-
+ocupar(pos(F,C),T) :- nth0(F,T,FILA), nth0(C,FILA,CELDA), CELDA=ocupada.
 
 %% Ejercicio 3
 %% vecino(+Pos, +Tablero, -PosVecino) será verdadero cuando PosVecino sea
 %% un átomo de la forma pos(F', C') y pos(F',C') sea una celda contigua a
 %% pos(F,C), donde Pos=pos(F,C). Las celdas contiguas puede ser a lo sumo cuatro
 %% dado que el robot se moverá en forma ortogonal.
-vecino(_,_,_).
+vecino(pos(F,C),T,pos(NORTE,C)) :-  NORTE is F-1, nth0(NORTE,T,FIL), nth0(C,FIL,_).
+vecino(pos(F,C),T,pos(SUR,C)) :-  SUR is F+1, nth0(SUR,T,FIL), nth0(C,FIL,_).
+vecino(pos(F,C),T,pos(F,ESTE)) :-  ESTE is C+1, nth0(F,T,FIL), nth0(ESTE,FIL,_).
+vecino(pos(F,C),T,pos(F,OESTE)) :-  OESTE is C-1, nth0(F,T,FIL), nth0(OESTE,FIL,_).
 
 %% Ejercicio 4
 %% vecinoLibre(+Pos, +Tablero, -PosVecino) idem vecino/3 pero además PosVecino
 %% debe ser una celda transitable (no ocupada) en el Tablero
-vecinoLibre(_,_,_).
+vecinoLibre(pos(F,C),T,pos(F2,C2)) :- vecino(pos(F,C),T,pos(F2,C2)), nth0(F2,T,FIL), nth0(C2,FIL,CEL), CEL\==ocupada.
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%
@@ -65,6 +67,7 @@ camino2(_,_,_,_).
 %% En este ejercicio se permiten el uso de predicados: dynamic/1, asserta/1, assertz/1 y retractall/1.
 camino3(_,_,_,_).
 
+
 %%%%%%%%%%%%%%%%%%%%%%%%
 %% Tableros simultáneos
 %%%%%%%%%%%%%%%%%%%%%%%%
@@ -75,3 +78,17 @@ camino3(_,_,_,_).
 %% sólo por celdas transitables de ambos tableros.
 %% Nota: Es posible una implementación que resuelva en forma inmediata casos en los que trivialmente no existe camino dual posible.
 caminoDual(_,_,_,_,_).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+%% Tableros de ejemplo
+%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Vacio de 3x3
+tablero(ej1,T) :- tablero(3,3,T).
+
+%% Ocupado solo el centro de 3x3
+tablero(ej2,T) :- tablero(3,3,T), ocupar(pos(1,1),T).
+
+%% Figura 1 de 5x5
+tablero(ej5x5,T) :- tablero(5,5,T), ocupar(pos(1,1),T), ocupar(pos(1,2),T).
